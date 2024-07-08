f95e67fd88d140909d14d5e005522a61d49ee940
Adalidh1 <adalidh141@gmail.com>
2024-07-08 16:54:05 -0600

    calculadoras

--- src/app/view/heroes/dashboard/dashboard.component.ts
+++ src/app/view/heroes/dashboard/dashboard.component.ts
@@ -3,13 +3,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatTooltipModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatTooltipModule, MatIconModule, MatMenuModule],
 })
 export class DashboardComponent {
 
